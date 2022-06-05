b33ae7b44addb7ed8c024ea05e30db2fc15fe46a
pavancodes24 <pavanchandupatla24@gmail.com>
2022-06-05 13:40:00 +0530

    added password recovery email backend.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,8 @@
 const User = require('../models/user');
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
-const sendToken = require('../utils/jwtToken')
+const sendToken = require('../utils/jwtToken');
+const sendEmail = require('../utils/sendEmail');
 
 // Register a user => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -56,6 +57,52 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     sendToken(user, 200, res);
   });
   
+// Forgot password => /api/v1/password/forgot
+exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findOne({ email:req.body.email });
+
+  if(!user){
+    return next(new ErrorHandler("Invalid Email or Password", 404));
+  }
+  // get rest token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({validateBeforeSave:false})
+
+  //createResetPasswordUrl
+  // const resetUrl =`${req.protocol}://${req.get('host')}/api/v1/password/reset/${resetToken}`;
+
+  // const message = `Your password reset token is as follow:\n\n\${resetUrl}\n\nIf you have not requested this email, please ignore it.`;
+  const resetUrl = `${req.protocol}://${req.get("host")}/api/v1/password/reset/${resetToken}`;
+
+  const message = `Your password reset token is as follow:\n\n${resetUrl}\n\nIf you have not requested this email, then  ignore it.`;
+
+  
+  try {
+     await sendEmail({
+       email: user.email,
+       subject: 'ShopIT password recovery',
+       message
+     })
+     res.status(200).json({
+       success: true,
+       message:`Email sent to: ${user.email}`
+     })
+  } catch (error) {
+    user.resetPasswordToken= undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({validateBeforeSave:false})
+
+    return next(new ErrorHandler(error.message,500))
+  }
+
+
+
+
+
+
+})
 
 // Logout user => /api/v1/logout
 exports.logout = catchAsyncErrors(async(req, res, next)=>{
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const {registerUser,loginUser, logout} = require('../controllers/authController');
+const {registerUser,loginUser, logout,forgotPassword} = require('../controllers/authController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+router.post('/password/forgot', forgotPassword);
+
 router.get('/logout',logout);
 
 
