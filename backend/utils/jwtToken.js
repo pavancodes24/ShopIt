0c21f0fa08abb18d0feac7c6fb85543c4edd5bd3
pavancodes24 <pavanchandupatla24@gmail.com>
2022-06-06 23:36:06 +0530

    adding order resources in backend

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -7,7 +7,7 @@ const sendToken = (user,statusCode,res)=>{
     //options for cookie
     const options = { 
         expires: new Date(
-            Date.now()+process.env.COOKIE_EXPIRES_TIME * 24*60*60*1000 //1day* 7
+            Date.now()+process.env.COOKIE_EXPIRES_TIME * 24*60*60*1000 //1day* 7(this is defined in the config env file)
             ),
             httpOnly: true
     }
