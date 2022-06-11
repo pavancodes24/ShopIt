d0faa384b79ea66b1075696fdde1386d0b102103
pavancodes24 <pavanchandupatla24@gmail.com>
2022-06-11 19:43:02 +0530

    login and logout implemented.

--- frontend/src/components/redux/actions/userActions.js
+++ frontend/src/components/redux/actions/userActions.js
@@ -11,6 +11,8 @@ import {
   LOAD_USER_REQUEST,
   LOAD_USER_SUCCESS,
   LOAD_USER_FAIL,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL
 
 } from "../constants/userConstants";
 
@@ -76,29 +78,6 @@ export const register = (userData) => async (dispatch) => {
     }
 }
 
-//Load user 
-// export const loadUser = () => async (dispatch) => {
-//     try {
-
-//         dispatch({ type: LOAD_USER_REQUEST})
-
-       
-
-//         const { data } = await axios.post('/api/v1/me')
-
-//         dispatch({
-//             type: LOAD_USER_SUCCESS,
-//             payload: data.user
-//         })
-
-//     } catch (error) {
-//         dispatch({
-//             type: LOAD_USER_FAIL,
-//             payload: error.response.data.message
-//         })
-//     }
-// }
-
 // Load user
 export const loadUser = () => async (dispatch) => {
     try {
@@ -119,3 +98,22 @@ export const loadUser = () => async (dispatch) => {
         })
     }
 }
+
+// Logout user
+export const logout = () => async (dispatch) => {
+    try {
+
+        await axios.get('/api/v1/logout')
+
+        dispatch({
+            type: LOGOUT_SUCCESS,
+        })
+
+    } catch (error) {
+        dispatch({
+            type: LOGOUT_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
--- frontend/src/components/redux/reducers/userReducers.js
+++ frontend/src/components/redux/reducers/userReducers.js
@@ -9,6 +9,8 @@ import {
   LOAD_USER_SUCCESS,
   LOAD_USER_REQUEST,
   LOAD_USER_FAIL,
+  LOGOUT_SUCCESS,
+  LOGOUT_FAIL
 } from "../constants/userConstants";
 
 export const authReducer = (state = { user: {} }, action) => {
@@ -45,6 +47,21 @@ export const authReducer = (state = { user: {} }, action) => {
         user: null,
         error: action.payload,
       };
+
+      case LOGOUT_SUCCESS:
+        return {
+          loading: false,
+          isAuthenticated: false,
+          user: null,
+        };
+  
+      case LOGOUT_FAIL:
+        return {
+          ...state,
+          error: action.payload,
+        };
+  
+
 
     case CLEAR_ERRORS:
       return {
